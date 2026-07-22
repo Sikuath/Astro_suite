7485326044774de952be8b0de35f24630c78eeb3
Sikuath <pum.up@orange.fr>
2026-07-22 16:30:51 +0200

    modif autobat

--- Astro_pretraitement.bat
+++ Astro_pretraitement.bat
@@ -1,16 +1,13 @@
 @echo off
 title Astro Pretraitement
 
-REM Se placer dans le dossier racine Astro_suite
-cd /d "%~dp0"
+REM Racine Astro_suite
+set ROOT=%~dp0
 
-REM Activer l'environnement virtuel commun
-call ".venv\Scripts\activate.bat"
+REM Aller dans le module
+cd /d "%ROOT%Astro_Pretraitement"
 
-REM Se placer dans Astro_Pretraitement
-cd /d "%~dp0Astro_Pretraitement"
-
-REM Lancer Streamlit avec le Python du venv
-python -m streamlit run app.py
+REM Lancer avec le Python du venv commun
+"%ROOT%.venv\Scripts\python.exe" -m streamlit run app.py
 
 pause
